05abd2dd94e16d10ba6b24cb7410e1414807dfd5
Kelton <kadey001@ucr.edu>
2019-05-29 20:20:08 -0700

    Updated to tasks
    Also testing speaker sounds

--- StackerGame/StackerGame/main.c
+++ StackerGame/StackerGame/main.c
@@ -14,7 +14,16 @@
 #include "shift_reg.c"
 
 #define uc unsigned char
+#define us unsigned short
+#define ul unsigned long
 
+#define NOTEC4 261.63
+#define NOTED4 293.66
+#define NOTEB4 493.88
+#define NOTEC5 523.25
+
+us success[2] = { NOTEB4, NOTEC5 };
+us fail[2] = { NOTED4, NOTEC4 };
 
 /*
 B0 = SER
@@ -28,37 +37,113 @@ C2 = Button 3
 C3 = Button 4
 */
 
+//===User Defined FSMs===
+enum soundState { OFF, SUCCESS, FAIL };
+
+int SMTick1(int state) {
+	
+	uc button1 = ~PINC & 0x01;
+	uc button2 = ~PINC & 0x02;
+	
+	switch(state) {
+		case OFF:
+			if(button1) {
+				state = SUCCESS;
+			}
+			else if(button2) {
+				state = FAIL;
+			}
+			else {
+				state = OFF;
+			}
+			break;
+		case SUCCESS: break;
+		case FAIL: break;
+	}
+	switch(state) {
+		case OFF: set_PWM(0); break;
+		case SUCCESS: 
+			set_PWM(success[0]);
+			while(!TimerFlag);
+			TimerFlag = 0;
+			set_PWM(success[1]);
+			while(!TimerFlag);
+			TimerFlag = 0;
+			state = OFF;
+			break;
+		case FAIL:
+			set_PWM(success[0]);
+			while(!TimerFlag);
+			TimerFlag = 0;
+			set_PWM(success[1]);
+			while(!TimerFlag);
+			TimerFlag = 0;
+			state = OFF;
+			break;
+	}
+	return state;
+}
+
 int main(void)
 {
-    DDRA = 0xFF; PORTA = 0x00;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0x00; PORTC = 0xFF;
-	DDRD = 0xFF; PORTD = 0x00;
+    DDRA = 0xFF; PORTA = 0x00; //LCD Control Lines
+	DDRB = 0xFF; PORTB = 0x00; //Shift Register
+	DDRC = 0x00; PORTC = 0xFF; //INPUTS
+	DDRD = 0xFF; PORTD = 0x00; //LCD Data Lines
+	
+	//Tasks Period
+	ul int SMTick1_calc = 200;
+	
+	
+	//Calculating GCD
+	ul int GCD = findGCD(SMTick1_calc, SMTick1_calc);
+	
+	
+	//Recalculate GCD periods for scheduler
+	ul int SMTick1_period = SMTick1_calc/GCD;
+	
+	
+	//Array of Tasks
+	static task task1;
+	task *tasks[] = { &task1 };
+	const us numTasks = sizeof(tasks)/sizeof(task*);
+	
+	//Task 1
+	task1.state = -1; //Init
+	task1.period = SMTick1_period;
+	task1.elapsedTime = SMTick1_period;
+	task1.TickFct = &SMTick1;
+	
 	
 	PWM_on();
 	TimerSet(50);
 	TimerOn();
 	
-	uc C0 = 0x00;
-	uc C1 = 0x00;
-	uc C2 = 0x00;
-	uc C3 = 0x00;
+	LCD_init();
+	LCD_DisplayString(1, "PRESS START");
+	
+	//uc C0 = 0x00;
+	//uc C1 = 0x00;
+	//uc C2 = 0x00;
 	
 	transmit_data(0xF0);
 	
+	us i; //Loop iterator
     while (1) 
     {
-		C0 = ~PINC & 0x01;
-		C1 = ~PINC & 0x02;
-		C2 = ~PINC & 0x04;
-		C3 = ~PINC & 0x08;
 		
-		if(C0) {
-			transmit_data(0x01);
+		//Scheduler code
+		for (i = 0; i < numTasks; i++) {
+			//Task ready to tick
+			if (tasks[i]->elapsedTime >= tasks[i]->period) {
+				//Setup next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				//Reset elapsed time for next tick
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime +=1;
 		}
-		else if(C1) {
-			clear_data();
-			//transmit_data(0x10);
-		}
+		while(!TimerFlag);
+		TimerFlag = 0;
 	}
 }
